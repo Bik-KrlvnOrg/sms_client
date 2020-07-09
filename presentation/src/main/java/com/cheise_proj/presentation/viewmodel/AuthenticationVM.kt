e3f56070dde3826291e16b26d42f8c8e0222af2c
bik_krlvn <kelvinbirikorang@gmail.com>
2020-07-09 19:29:01 +0000

    [FEATURE] AuthVM
    Added files on presentation package
    * AuthenticationVM
    * User
    * UserMapper
    * BaseViewModel
    * extension on User for UserMapper
    * dependencies.gradle
    
    resolves: #4

---
+++ presentation/src/main/java/com/cheise_proj/presentation/viewmodel/AuthenticationVM.kt
@@ -0,0 +1,31 @@
+package com.cheise_proj.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.toLiveData
+import com.cheise_proj.domain.usecases.user.AuthenticationTask
+import com.cheise_proj.presentation.extensions.toModel
+import com.cheise_proj.presentation.model.Resource
+import com.cheise_proj.presentation.model.User
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Observable
+import javax.inject.Inject
+
+class AuthenticationVM @Inject constructor(private val authenticationTask: AuthenticationTask) :
+    BaseViewModel() {
+
+    var userResource: LiveData<Resource<User>> = MutableLiveData()
+
+    fun authenticateWithCredentials(username: String, password: String) {
+        userResource =
+            authenticationTask.execute(AuthenticationTask.Params(username, password))
+                .map { it.toModel() }
+                .map { Resource.success(it) }
+                .startWith(Observable.just(Resource.loading()))
+                .onErrorResumeNext { Observable.just(Resource.error(it.localizedMessage)) }
+                .toFlowable(BackpressureStrategy.LATEST).toLiveData()
+    }
+
+
+}
+
